d8dda15f470c43d68fb11754a573b55942f01b57
vib1910601 <vib1910601@student.ctu.edu.vn>
2023-10-06 14:07:49 +0700

    Xay dung file manager bang php

---
+++ includes/Make.php
@@ -0,0 +1,59 @@
+<?php
+
+class Make
+{
+    public static function create_file($parentDir, $filename, $data = '')
+    {
+        $path = _DATA_DIR . '/' . $parentDir . '/' . $filename;
+        file_put_contents($path, $data);
+    }   
+
+    public static function create_Folder($parentDir, $foldername)
+    {
+        $path = _DATA_DIR . '/' . $parentDir . '/' . $foldername;
+        mkdir($path);
+    }
+
+    public static function rename($parentDir, $old, $name)
+    {
+        $oldPath = _DATA_DIR . '/' . $parentDir . '/' . $old;
+        $newPath = _DATA_DIR . '/' . $parentDir . '/' . $name;
+        rename($oldPath, $newPath);
+    }
+
+    public static function deleteFile($parentDir, $filename)
+    {
+        $path = _DATA_DIR . '/' . $parentDir . '/' . $filename;
+        if (file_exists($path)) {
+            unlink($path);
+            return true;
+        }
+        return false;
+    }
+
+    public static function deleteFolder($parentDir, $name)
+    {
+        $path = _DATA_DIR . $parentDir . '/' . $name;
+        if (is_dir($path)) {
+            $load = new Load();
+            $dataArr = $load->scanDir($parentDir . '/' . $name);
+            if (!empty($dataArr)) {
+                foreach ($dataArr as $item) {
+                    $pathChildren = $parentDir . '/' . $name . '/' . $item;
+                    if ($load->isType(_DATA_DIR . $pathChildren) == 'file') {
+                        self::deleteFile(dirname($pathChildren), $item);
+                    } else {
+                        self::deleteFolder($parentDir . '/' . $name, $item);
+                    }
+                }
+            }
+            rmdir($path);
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+?>
